57d199ad4f1cc449d47e7c86d32d9f6a1979cb8e
Kevin Jordan <kej@kyrefre.com>
2024-03-19 12:57:19 -0400

    Document and genericize for MacOS and Linux

--- cray-build.sh
+++ cray-build.sh
@@ -1,2 +1,28 @@
 #!/bin/sh
-CC=gcc-13 __crayxmp=1 __cos=1 CRAYXMP_COS=1 PATH=~/Dropbox/Simulators/Cray/COS-Tools:$PATH gmake $*
+#
+#  This script builds ACK for the Cray X-MP computer running
+#  the COS operating system. The COS Tools from the folling
+#  GitHub repository must be built, installed, and accessible
+#  via the PATH variable before running this script:
+#
+#    https://github.com/kej715/COS-Tools
+#
+#  In addition, when building on MacOS, true gcc must be
+#  installed because ACK will not build successfully using
+#  LLVM. Change the setting of the CC variable, below, as
+#  needed to correspond with the version of gcc installed.
+#
+#  To build ACK:
+#
+#    ./cray-build.sh
+#
+#  To install it:
+#
+#    sudo ./cray-build.sh install
+#
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export CC=gcc-13
+else
+  export CC=gcc
+fi
+__crayxmp=1 __cos=1 CRAYXMP_COS=1 gmake $*
