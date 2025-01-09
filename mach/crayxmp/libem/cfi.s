9c362375d7e780c15e0b34bfbb3f08b62e4b7a64
Kevin Jordan <kej@kyrefre.com>
2025-01-08 21:33:32 -0500

    Support floating point values that map to integer values wider than 48 bits

--- mach/crayxmp/libem/cfi.s
+++ mach/crayxmp/libem/cfi.s
@@ -20,7 +20,7 @@ text:    section   code
          s1        ,a7            ; pop float to convert
          a7        a7+1
          s0        s1
-         jsz       2f             ; if value is 0
+         jsz       3f             ; if value is 0
          s2        <15            ; exponent mask
          s3        s1
          s3        s3>48
@@ -39,20 +39,31 @@ text:    section   code
          s2        o'40060        ; calculate shift count for mantissa
          s2        s2-s3
          s0        s2
-         jsm       3f             ; if value too large
+         jsm       4f             ; if value greater than 2**48
          a1        s2
          s3        <48            ; isolate mantissa
          s7        s3&s1
          s7        s7>a1          ; de-normalize it
-         s0        s1
-         jsp       1f             ; if original float was positive
-         s7        -s7
 1:
-         j         b00
+         s0        s1
+         jsp       2f             ; if original float was positive
+         s7        -s7
 2:
-         s7        s1
          j         b00
 3:
+         s7        s1
+         j         b00
+4:
+         s2        -s2            ; calculate count for left shift
+         s3        16
+         s0        s2-s3
+         jsm       5f             ; if value in range
          j         @erange
+5:
+         a1        s2
+         s3        <48            ; isolate mantissa
+         s7        s3&s1
+         s7        s7<a1          ; de-normalize it
+         j         1b
 
          end
